60a97fd2f5e74e7e15fd02fa9c78c55f0fcf8e52
silvaRedG <matheus.silva@redgtech.com.br>
2023-11-02 22:55:23 -0300

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,44 @@
+import { v4 as uuidv4 } from 'uuid';
+
+
+/* eslint-disable react/jsx-key */
 function App() {
+  /*Codigo Java Script */
 
+const list = [
+  {id: uuidv4(), task:"tarefa"},
+  {id: uuidv4(), task: "tarefa b"}
+]
+
+  function inputMudou(event) {
+    console.log(event.target.value);
+  }
+
+  function cliqueiNoBotao() {
+    console.log("click");
+  }
+
+  /*Retorna HTML*/
   return (
-
     <div>
-    
-    Hello React Now
-      
-      </div>
-  )
+      <input
+        type="text"
+        placeholder="o que tenho que fazer?"
+        autoFocus
+        onChange={inputMudou}
+      />
+      <button onClick={cliqueiNoBotao}>Adicionar</button>
+
+      <ul>
+       {
+        list.map(item =>(
+          <li key={item.id}>{item.task}</li>
+          
+        ))
+       }
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
