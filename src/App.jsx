848359e850d976e4d0995532bce11646c5b6da1e
silvaRedG <matheus.silva@redgtech.com.br>
2023-11-09 12:07:25 -0300

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,169 @@
 import { v4 as uuidv4 } from "uuid";
-import { useState } from "react";
-import { Container, List, H1, Input, Button, Li,ListItem } from "./styles.js";
-import { FcEmptyTrash, FcCheckmark, } from "react-icons/fc";
+import { useState, useEffect } from "react";
+import {
+  Container,
+  List,
+  H1,
+  Input,
+  Button,
+  Li,
+  Ul,
+  ListItem,
+  Trash,
+  Check,
+  Edit,
+} from "./styles.js";
 
-/* eslint-disable react/jsx-key */
+// Gravar a lista localmente ou seja sempre atualizar o navegador a lista permanece
+const getLocalStorage = () => {
+  let list = localStorage.getItem('list')
+  if (list) {
+    return (list = JSON.parse(localStorage.getItem('list')))
+  } else {
+    return []
+  }
+}
+
+
 function App() {
-  /*Codigo Java Script */
-
-  const [list, setList] = useState([
-    {
-      id: uuidv4(),
-      task: (""),
-    },
-  ]);
+  const [list, setList] = useState(getLocalStorage());
   const [task, setTask] = useState("");
-
-  // estrutura do estado useState -> nome variavel,nome pra alterar a variavel -> formato padrão pra exibir na tela
+  const [editingTaskId, setEditingTaskId] = useState(null);
+  const [editTask, setEditTask] = useState("");
 
   function inputMudou(event) {
-    console.log(event.target.value);
-
-    setTask(event.target.value); // vai add dentro do array um task que tá escrito dentro do input e alterando com setList
-
-    console.log(task);
+    setTask(event.target.value);
   }
 
+  // Inserir tarefa ao apertar o enter
+  function handleKeyDown(event) {
+    if (event.key === "Enter") {
+      cliqueiNoBotao();
+    }
+  }
+
+  useEffect(() => {
+    localStorage.setItem('list', JSON.stringify(list))
+  }, [list])
+
+  //Adicionar Lista
   function cliqueiNoBotao() {
-    setList([...list, { id: uuidv4(), task }]);
+    if (task) {
+      setList([...list, { id: uuidv4(), task, finished: false }]);
+      setTask("");
+    }else{
+      alert("Digite uma tarefa")
+    }
+    
+  }
+// Função na qual vai ser criada a edição da tarefa
+  function editarTarefa(id) {
+    setEditingTaskId(id);
+    const taskToEdit = list.find((item) => item.id === id);
+    if (taskToEdit) {
+      setEditTask(taskToEdit.task);
+    }
   }
 
-  /*Retorna codigo HTML*/
+  // Função pra salvar a tarefa
+  function salvarEdicao(id) {
+
+
+    if (editTask.trim() === '') {
+      alert("O campo não pode ficar vazio")
+    }else{
+
+      const updatedList = list.map((item) =>
+      item.id === id ? { ...item, task: editTask } : item
+    );
+    setList(updatedList);
+    setEditingTaskId(null);
+    
+    }
+
+    
+  }
+
+  // Função que cancela a edição da tarefa
+  function cancelarEdicao() {
+    setEditingTaskId(null);
+    setEditTask("");
+  }
+
+  useEffect(() => {
+    const storedTasks = JSON.parse(localStorage.getItem("taskList"));
+    if (storedTasks) {
+      setList(storedTasks);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("taskList", JSON.stringify(list));
+  }, [list]);
+
+  function finishTask(id) {
+    const newList = list.map((item) =>
+      item.id === id ? { ...item, finished: !item.finished } : item
+    );
+
+    setList(newList);
+  }
+
+  function deleteItem(id) {
+    const newList = list.filter((item) => item.id !== id);
+    setList(newList);
+  }
+
   return (
     <Container>
       <List>
         <H1>React Agenda</H1>
         <Input
           type="text"
-          placeholder="o que tenho que fazer?"
+          placeholder="Minha rotina"
           autoFocus
+          value={task}
           onChange={inputMudou}
+          onKeyDown={handleKeyDown}
         />
-
         <Button onClick={cliqueiNoBotao}>Adicionar</Button>
 
-        <ul>
-          {list.map((item) => (
-            <ListItem>
-              <FcCheckmark />
-              <Li key={item.id}>{item.task}</Li>
-              <FcEmptyTrash />
-            </ListItem>
-          ))}
-        </ul>
+        <Ul>
+          {list.length > 0 ? (
+            list.map((item) => (
+              <ListItem isFinished={item.finished} key={item.id}>
+                {item.id === editingTaskId ? (
+                  
+                  <>
+                    <input
+                      type="text"
+                      value={editTask}
+                      onChange={(e) => setEditTask(e.target.value)}
+                      
+                    />
+                    <Button onClick={() => salvarEdicao(item.id)}>
+                      Salvar
+                    </Button>
+                    <Button onClick={cancelarEdicao}>Cancelar</Button>
+                    
+                  </>
+                ) : (
+                  
+                  <>
+                      <Li>{item.task}</Li>
+                    <Check onClick={() => finishTask(item.id)} />
+                    
+                    <Edit onClick={() => editarTarefa(item.id)}/>
+                    <Trash onClick={() => deleteItem(item.id)} />
+                  </>
+                  
+                )}
+              </ListItem>
+            ))
+          ) : (
+            <h3>Sem Tarefas</h3>
+          )}
+        </Ul>
       </List>
     </Container>
   );
--- src/styles.js
+++ src/styles.js
@@ -1,34 +1,35 @@
 // @ts-nocheck
 import styled from "styled-components";
+import { FcEmptyTrash, FcCheckmark } from "react-icons/fc"; // editando icones personalizado
+import { CiEdit } from "react-icons/ci";
 
 export const Container = styled.div`
-  background-image: radial-gradient(
-    circle at 44.68% 30.16%,
-    #6e7070 0,
-    #5c6563 16.67%,
-    #455655 33.33%,
-    #284444 50%,
-    #033335 66.67%,
-    #00252a 83.33%,
-    #001c23 100%
-  );
-
   width: 100vw;
   height: 100vh;
 
   display: flex;
   justify-content: center;
   align-items: center;
+
+  background: url(https://source.unsplash.com/1600x900/?landscape&_gl=1*7ad761*_ga*MTI3Njc3NjAxNy4xNjc1NDc2MDk3*_ga_37GXT4VGQK*MTY4OTgxNjMwOC4xNjEuMS4xNjg5ODE2NDA1LjAuMC4w);
+  background-size: cover;
 `;
 
 export const List = styled.div`
   background: #030328;
   padding: 30px 20px;
   border-radius: 20px;
+  height: 90vh;
+  overflow-y: auto;
 
   ul {
     padding: 0;
     margin-top: 60px;
+  }
+
+  h3 {
+    color: #ffffff;
+    text-align: center;
   }
 `;
 
@@ -61,6 +62,7 @@ export const Button = styled.button`
     background-color: #ffffff;
     color: black;
     text-decoration: none;
+    opacity: 0.8;
   }
 `;
 
@@ -70,21 +72,50 @@ export const Li = styled.li`
   font-weight: bold;
 `;
 
+export const Ul = styled.ul`
+  text-align: center;
+  padding: 0;
+  list-style: none;
+`;
+
 export const ListItem = styled.div`
   display: flex;
-  justify-content: space-between;
+  justify-content: flex-end;
   align-items: center;
+  gap: 10px;
 
   border-radius: 5px;
-  background: #e4e4e4;
+  background: ${(props) => (props.isFinished ? "#008000" : "#e4e4e4")};
   box-shadow: 1px 4px 10px 0px rgba(0, 0, 0, 0.2);
-  height: 60px;
+  height: 70px;
   margin-bottom: 30px;
   padding: 0 20px;
 
   li {
     list-style: none;
-    color: black;
+    color: ${(props) => (props.isFinished ? "#ffffff" : "#000000")};
     font-size: 15px;
+    text-decoration: ${(props) =>
+      props.isFinished ? "line-through" : "#e4e4e4"};
+    display: block;
+    position: sticky;
+    text-align: right;
+    right: 48%;
   }
+`;
+// editando icones personalizado,não elementos nativos e por isso fazemos dessa maneira
+export const Trash = styled(FcEmptyTrash)`
+  cursor: pointer;
+  font-size: 2em;
+`;
+export const Check = styled(FcCheckmark)`
+  position: sticky;
+  right: 90%;
+
+  cursor: pointer;
+  font-size: 2em;
+`;
+export const Edit = styled(CiEdit)`
+  cursor: pointer;
+  font-size: 2em;
 `;
